bcc64252c803658945ea1542e6cefe9a2c4078dd
Joares <joaresmiranda@gmail.com>
2023-10-24 17:02:00 -0300

    Seção Sobre a escola

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'about-pattern': "url('./assets/images/diagonal-laranja.png')",
+      },
+    },
   },
   plugins: [],
 };
